24e9c0f1e1885b2509ae5821af8f5f1267b6b2da
Kevin <kevin005005@gmail.com>
2023-06-30 02:22:10 +0200

    Blocking example using chrome.webNavigation and chrome.tabs

---
+++ service_worker.js
@@ -0,0 +1,13 @@
+block_redirect_url = new URL("https://asset.kompas.com/crops/S3WIl-K0PbO48leil75gqLjKgvM=/177x0:1545x912/750x500/data/photo/2022/08/01/62e75917161c4.png");
+
+var blocked_websites = [new URL("https://www.youtube.com/watch?v=g6rFff2MAxM"), new URL("https://www.duolingo.com/")];
+
+// We can compare if the URL's host is equal to the host of one of the block websites
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    let url = new URL(details.url);
+    console.log(url.hostname);
+    if (blocked_websites.find(website => website.hostname == url.hostname)) {
+        console.log("This URL is blocked.");
+        chrome.tabs.update({url: block_redirect_url.href});
+    }
+});
