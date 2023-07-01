626b48588bf4fc6ec178754080c1e24fc1fd28f6
Kevin <kevin005005@gmail.com>
2023-07-01 21:55:32 +0200

    Updated the redirect

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-block_redirect_url = new URL("https://asset.kompas.com/crops/S3WIl-K0PbO48leil75gqLjKgvM=/177x0:1545x912/750x500/data/photo/2022/08/01/62e75917161c4.png");
+block_redirect_url = 'reasons/unblock.html';
 
 var blocked_websites = [new URL("https://www.youtube.com/watch?v=g6rFff2MAxM"), new URL("https://www.duolingo.com/")];
 
@@ -8,6 +8,6 @@ chrome.webNavigation.onBeforeNavigate.addListener((details) => {
     console.log(url.hostname);
     if (details.frameId == 0 && blocked_websites.find(website => website.hostname == url.hostname)) {
         console.log("This URL is blocked.");
-        chrome.tabs.update({url: block_redirect_url.href});
+        chrome.tabs.update({url: block_redirect_url});
     }
 });
